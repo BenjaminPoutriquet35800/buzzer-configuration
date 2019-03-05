8cb7aad6cf30668ef8c1cd1fa315c2c7431198f6
Poutriquet Benjamin <benjaminpoutriquet@hotmail.fr>
2019-03-05 18:37:46 +0100

    BPO : Ajout des icons materials

---
+++ app/views/main/main.js
@@ -0,0 +1,11 @@
+/**
+ * Se charge d'initialiser les compteurs de caractères
+ * Du formulaire de configuration
+ */
+const initCharacterCounterInputs = function () {
+    $('input#input_text, textarea#textarea2').characterCounter();
+}
+
+initCharacterCounterInputs();
+
+
